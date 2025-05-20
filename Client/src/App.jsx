afdae3a3a9353f2c3caab97bc1f5be3c5d56706f
Rattini <rattinicursods@gmail.com>
2025-05-20 09:31:08 -0300

    Começo tela de categorias

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,12 +3,14 @@ import Cardegoria from "./components/Cardegorias/Cardegoria";
 import "./App.css";
 import ListaItens from "./pages/ListaItens";
 import ItemCarrinho from "./components/ItemCarrinho";
+import Categorias from "./pages/Categorias";
+
 
 
 const App = () => {
   return (
     <div>
-      <Header />
+      <Categorias />
     </div>
   );
 };
---
+++ Client/src/components/CardCategs/CardCateg.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const CardCateg = () => {
+  return (
+    <div>
+        <div>
+            <button className='col-6 me-3 border-0 p-5 rounded-4'>CU <img src='https://placehold.co/350x307'></img> </button>
+            <button className='col-4 border-0 p-5 rounded-4'> <img src='https://placehold.co/366x307'></img> </button>
+        </div>
+
+        <div className='mt-3 '>
+            <button className='col-4 me-3 border-0 p-5 rounded-4'>CU <img src='https://placehold.co/366x307'></img> </button>
+            <button className='col-6 border-0 p-5 rounded-4'>  <img src='https://placehold.co/262x307'></img> </button>
+        </div>
+    </div>
+  )
+}
+
+export default CardCateg
