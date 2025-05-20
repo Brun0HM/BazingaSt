1b166affae25ad0778a21ccfbf1cf76920cf2166
Brun0 <brunodev41@gmail.com>
2025-05-20 16:59:05 -0300

    add Home component with layout and styles; integrate Superman image

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,11 +4,13 @@ import "./App.css";
 import ListaItens from "./pages/ListaItens";
 import ItemCarrinho from "./components/ItemCarrinho";
 import Header from "./components/header/Header.jsx";
+import Home from "./pages/Home.jsx";
+import SuperMan from "./assets/SUPERMAN.png";
 
 const App = () => {
   return (
     <div>
-      <Header />
+      <Home superMan={SuperMan} />
     </div>
   );
 };
--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import Header from "../components/header/Header.jsx";
 
-const Home = () => {
-  return <div>Home</div>;
-};
+const Home = ({ superMan }) => (
+  <div className="fundido">
+    {/* Header sem position absolute */}
+    <Header />
+
+    {/* Container empurrado para baixo */}
+    <div className="container position-relative pt-5">
+      <div className="row align-items-center">
+        {/* Coluna de texto */}
+        <div className="col-12 col-md-6 text-center text-md-start mb-4 mb-md-0">
+          <h1 className="fw-bold display-2 text-white">
+            NOVA <br />
+            <span>COLEÇÃO!</span>
+          </h1>
+          <div className="d-flex gap-3 justify-content-center justify-content-md-start mt-4">
+            <img
+              src="https://placehold.co/200x200"
+              alt="Marca 1"
+              className="img-fluid"
+            />
+            <img
+              src="https://placehold.co/200x200"
+              alt="Marca 2"
+              className="img-fluid"
+            />
+          </div>
+        </div>
+
+        {/* Coluna da imagem */}
+        <div className="col-12 col-md-6 d-flex justify-content-center">
+          <img
+            src={superMan}
+            alt="Superman LEGO"
+            className="img-fluid superman-img"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default Home;
