aa265ba0a1e7edad73d6aafd2acdeee5dd18c014
Thiago Mazzi <thiagodev728@gmail.com>
2025-05-21 10:25:01 -0300

    Add user configuration page and update App component structure

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,20 +2,16 @@ import React from "react";
 import Cardegoria from "./components/Cardegorias/Cardegoria";
 import "./App.css";
 import ListaItens from "./pages/ListaItens";
-import ItemCarrinho from "./components/ItemCarrinho";
 import Categorias from "./pages/Categorias";
 import Header from "./components/header/Header.jsx";
 import Checkout from "./components/Checkout/Checkout";
 import Cart from "./pages/Cart";
-
+import Config from "./pages/Config.jsx";
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-
-  );
+  return <div>
+    <Config/>
+  </div>;
 };
 
 export default App;
