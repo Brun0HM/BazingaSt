b5a958b622853be93d89ddcd776ffded95cd26fd
Brun0 <brunodev41@gmail.com>
2025-05-27 08:21:42 -0300

    Feito footer com responsividade

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -8,12 +8,12 @@ import Home from "./pages/Home.jsx";
 import SuperMan from "./assets/SUPERMAN.png";
 import Categorias from "./pages/Categorias";
 import DestaquePage from "./pages/DestaquePage";
-
-
+import Footer from "./components/footer/Footer.jsx";
 
 const App = () => {
   return (
     <div>
+      <Footer />
     </div>
   );
 };
