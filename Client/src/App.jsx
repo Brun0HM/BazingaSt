0d08e3bef3121855c685e40c4aecb645c3930d39
Thiago Mazzi <thiagodev728@gmail.com>
2025-06-04 14:03:04 -0300

    Remove unused imports from App component to clean up code

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,16 +2,6 @@ import "./App.css";
 import React from "react";
 import Home from "./pages/Home.jsx";
 import SuperMan from "./assets/supermannn.png";
-import DestaquePage from "./pages/DestaquePage";
-import RegisterCard from "./components/RegisterCard/RegisterCard.jsx";
-import RegisteredCards from "./components/RegisteredCards/RegisteredCards.jsx";
-import Payment from "./pages/Payment.jsx";
-import Footer from "./components/footer/Footer.jsx";
-import LoginPage from "./pages/LoginPage";
-import Logar from "./pages/Logar";
-import Login from "./components/Login/Login.jsx";
-import CreateAccount from "./pages/CreateAccount";
-import MainDashBoard from "./pages/MainDashBoard.jsx";
 import DBProdutos from "./pages/DBProdutos.jsx";
 
 const App = () => {
